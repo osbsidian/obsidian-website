9299e0810df92227e299adbfed832073ad1d0e2e
TR1ST4N <tv.courriel@gmail.com>
2025-10-09 03:11:10 +0200

    add tiktok verification 1/2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-quickstart';
+
+  fileName: string = 'tiktokgzVkL92x8O3AqR0pBk1JOqhU9Qqfl4nT.txt';
+  fileContent: string = 'tiktok-developers-site-verification=gzVkL92x8O3AqR0pBk1JOqhU9Qqfl4nT';
+
+  downloadTextFile() {
+    const content = this.fileContent;
+    const blob = new Blob([content], { type: 'text/plain' });
+    const url = window.URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = this.fileName;
+    a.click();
+
+    window.URL.revokeObjectURL(url);
+  }
 }
