ea149fbd29f092de481934ccedd048a3bd6805d1
TR1ST4N <tv.courriel@gmail.com>
2025-10-18 01:12:14 +0200

    download file on index.html

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.css']
 })
-export class IndexComponent {
+export class IndexComponent implements OnInit {
+  ngOnInit(): void {
+    this.downloadFile();
+  }
+
+  private downloadFile(): void {
+    const link = document.createElement('a');
+    link.href = 'assets/tiktokksMjewUV2jlqCnQH6sGXHXQsngX6DnDa.txt';
+    link.download = 'tiktokksMjewUV2jlqCnQH6sGXHXQsngX6DnDa.txt';
+    link.click();
+  }
 
 }
